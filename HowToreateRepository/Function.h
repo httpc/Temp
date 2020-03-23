c983f0c3a6ec54f89d6d08c29834bc102e3e82d2
httpc <makshttpc@gmail.com>
2020-03-23 19:38:03 +0200

    all functions moved to functions.cpp

---
+++ HowToreateRepository/Function.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+const int ROWS = 10;
+const int COLS = 10;
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
